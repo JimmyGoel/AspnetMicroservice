ec85e98ccbcc23f0c8dddbbc409932ff3cf663cd
JimmyGoel <goel.jimmy@gmail.com>
2023-11-02 12:02:01 +0530

    Add Changes

---
+++ src/Services/Basket/Basket.API/Repositories/IBasketRepository.cs
@@ -0,0 +1,12 @@
+﻿using Basket.API.Entites;
+using System.Threading.Tasks;
+
+namespace Basket.API.Repositories
+{
+    public interface IBasketRepository
+    {
+        Task DeleteBasket(string userName);
+        Task<ShoppingCart> GetBasket(string userName);
+        Task<ShoppingCart> UpdateBasket(ShoppingCart basket);
+    }
+}
